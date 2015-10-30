a3002efa0d666ce74c47b2ed0c5d4e6c98fbade8
Braintree <code@getbraintree.com>
2015-10-30 20:59:15 +0000

    First commit

---
+++ main.js
@@ -0,0 +1,60 @@
+import History from './lib/history';
+import Subscriber from './lib/subscriber';
+import Inputs from './inputs.json';
+
+let InputManager;
+
+function on(inputName) {
+  var subscriber = new Subscriber(inputName);
+
+  this.subscribers.push(subscriber);
+
+  return subscriber;
+}
+
+function evaluateSubscribers() {
+  this.subscribers.forEach((subscriber) => {
+    subscriber.evaluate();
+  });
+}
+
+InputManager = {
+  subscribers: [],
+  history: new History(),
+  on: on,
+  evaluateSubscribers: evaluateSubscribers
+};
+
+window.addEventListener('keydown', function (event) {
+  let keyCode = event.keyCode || event.key;
+
+  InputManger.history.record({
+    altKey: event.altKey,
+    ctrlKey: event.ctrlKey,
+    inputName: INPUTS[keyCode],
+    keyCode: event.keyCode,
+    metaKey: event.metaKey,
+    repeat: event.repeat,
+    shiftKey: event.shiftKey,
+    timeStamp: new Date(event.timestamp),
+    direction: 'down'
+  });
+});
+
+window.addEventListener('keyup', function (event) {
+  let keyCode = event.keyCode || event.key;
+
+  InputManger.history.record({
+    altKey: event.altKey,
+    ctrlKey: event.ctrlKey,
+    inputName: INPUTS[keyCode],
+    keyCode: event.keyCode,
+    metaKey: event.metaKey,
+    repeat: event.repeat,
+    shiftKey: event.shiftKey,
+    timeStamp: new Date(event.timestamp),
+    direction: 'up'
+  });
+});
+
+export default InputManager;
